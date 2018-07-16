809c4637a6c980c32999bb4ed48300986d32c157
Ashton <ashchristie@gmail.com>
2018-07-15 20:45:23 -0700

    finished tuts

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -14,6 +14,19 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+// Optional Error logging app
+// import Raven from 'raven-js';
+// import { sentry_url } from './data/config';
+
+// Raven.config(sentry_url, {
+//   tags: {
+//     git_commit: 'asdfkl;asfda',
+//     userLevel: 'editor'
+//   }
+// }).install();
+
+// console.log(window.doesNotExist.nope);
+
 const router = (
   <Provider store={store}>
     {/* Provider tag exposes store to entire application */}
